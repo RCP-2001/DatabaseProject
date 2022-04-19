94a2a96e71f198044b8f8d4a9a0515636cc75876
RCP-2001 <coalgolem.xd@gmail.com>
2022-04-18 20:12:48 -0700

    Big Boi Commit

--- poj/CS360/urls.py
+++ poj/CS360/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path('WebStore/', include('WebStore.urls'))
 
 ]
