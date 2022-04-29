b0774f6162108952974d083dc7db472f8a6fdbd3
RCP-2001 <coalgolem.xd@gmail.com>
2022-04-29 14:25:19 -0700

    Some updates + a test

--- poj/WebStore/models.py
+++ poj/WebStore/models.py
@@ -15,6 +15,20 @@ class CustomerWishList(models.Model):
     AmmountOfItem = models.IntegerField()
     Price = models.DecimalField(decimal_places=2, max_digits=9)
 
+class WishListPhone(models.Model):
+    WishID = models.ForignKey(CustomerWishList, on_delete=models.CASCADE)
+    Brand = models.CharField(max_length=200)
+    Quality = models.CharField(max_length=200)
+    Generation = models.IntegerField()
+    CameraQuality = models.IntegerField()
+    PriorityList = models.IntegerField()
+
+class WishListTV(models.Model):
+    WishID = models.ForignKey(CustomerWishList, on_delete=models.CASCADE)
+    Brand = models.CharField(max_length=200)
+    Hertz = models.IntegerField()
+    
+
 
 
 
