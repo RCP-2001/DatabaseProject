ce3891e209050cd055c0f0a917e8f69c44bdac4b
RCP-2001 <coalgolem.xd@gmail.com>
2022-04-29 16:21:05 -0700

    Added Tables

--- poj/WebStore/admin.py
+++ poj/WebStore/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 
-from .models import Customers, CustomerWishList
+from .models import Customers, CustomerWishList, WishListPhone, WishListTV, Vender, Products, Service, InternetServices, PhoneServices, Phones, TVs
 
 admin.site.register(Customers)
 admin.site.register(CustomerWishList)
+admin.site.register(WishListPhone)
+admin.site.register(WishListTV)
+admin.site.register(Vender)
+admin.site.register(Products)
+admin.site.register(Service)
+admin.site.register(InternetServices)
+admin.site.register(PhoneServices)
+admin.site.register(Phones)
+admin.site.register(TVs)
--- poj/WebStore/models.py
+++ poj/WebStore/models.py
@@ -16,7 +16,7 @@ class CustomerWishList(models.Model):
     Price = models.DecimalField(decimal_places=2, max_digits=9)
 
 class WishListPhone(models.Model):
-    WishID = models.ForignKey(CustomerWishList, on_delete=models.CASCADE)
+    WishID = models.ForeignKey(CustomerWishList, on_delete=models.CASCADE)
     Brand = models.CharField(max_length=200)
     Quality = models.CharField(max_length=200)
     Generation = models.IntegerField()
@@ -24,11 +24,59 @@ class WishListPhone(models.Model):
     PriorityList = models.IntegerField()
 
 class WishListTV(models.Model):
-    WishID = models.ForignKey(CustomerWishList, on_delete=models.CASCADE)
+    WishID = models.ForeignKey(CustomerWishList, on_delete=models.CASCADE)
     Brand = models.CharField(max_length=200)
     Hertz = models.IntegerField()
-    
+    ScreenType = models.CharField(max_length=200)
+    ScreenSize = models.IntegerField()
+    PriorityList = models.IntegerField()
 
+class Vender(models.Model):
+    VenderID = models.IntegerField(primary_key = True)
+    Name = models.CharField(max_length = 200)
 
+class Products(models.Model):
+    ProductID = models.IntegerField(primary_key = True)
+    Description = models.CharField(max_length=200)
+    VenderID = models.ForeignKey(Vender, on_delete=models.CASCADE)
+    Price = models.DecimalField(decimal_places=2, max_digits=9)    
+    DeliveryCost = models.DecimalField(decimal_places =2, max_digits=9)
+    DeliveryLength = models.IntegerField()
+    Type = models.CharField(max_length=200)
 
+class Service(models.Model):
+    ServiceID = models.IntegerField(primary_key = True)
+    Name = models.CharField(max_length=200)
+    Type = models.CharField(max_length=200)
+    Description = models.CharField(max_length=500)
+    SubscriptionPrice = models.DecimalField(decimal_places=2, max_digits=9)
+    SubscriptionLength = models.IntegerField()
+    ContractLength = models.IntegerField()
+    VenderID = models.ForeignKey(Vender, on_delete=models.CASCADE)
 
+class InternetServices(models.Model):
+    ServiceID = models.ForeignKey(Service, on_delete=models.CASCADE)
+    Speed = models.IntegerField()
+    DataCap = models.IntegerField()
+    IncludesRouter = models.BooleanField()
+
+class PhoneServices(models.Model):
+    ServiceID = models.ForeignKey(Service, on_delete=models.CASCADE)
+    Lines = models.IntegerField
+    DataCap = models.IntegerField()
+    HotSpot = models.BooleanField()
+    Speed = models.IntegerField()
+
+class Phones(models.Model):
+    ProductID = models.ForeignKey(Products, on_delete=models.CASCADE)
+    Brand = models.CharField(max_length=200)
+    Quality = models.CharField(max_length=200)
+    Generation = models.IntegerField()
+    CameraQuality = models.IntegerField()
+
+class TVs(models.Model):
+    ProductID = models.ForeignKey(Products, on_delete=models.CASCADE)
+    Brand = models.CharField(max_length=200)
+    Hertz = models.IntegerField()
+    ScreenType = models.CharField(max_length=200)
+    ScreenSize = models.IntegerField()
